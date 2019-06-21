eac13ca5de1bc3b5fa8bd843b3cd193e2ff26cc6
PaulDeveraux <sylwek.g@interia.pl>
2019-06-21 22:35:06 +0200

    Zmodyfikowany szablon do wyświetlania postów z bazy

--- blog/views.py
+++ blog/views.py
@@ -1,6 +1,8 @@
 from django.shortcuts import render
+from .models import Post
+from django.utils import timezone
 
-# Create your views here.
 
 def post_list(request):
-	return render(request, 'blog/post_list.html', {})
+	posts = Post.objects.filter(data_publikacji__lte=timezone.now()).order_by('data_publikacji')
+	return render(request, 'blog/post_list.html', {'posts': posts})
